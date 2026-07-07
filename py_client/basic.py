4f64d39f34b028e44ffb9648bd840cf381fbd254
kkamara <kamaracomputers@gmail.com>
2026-07-07 15:21:50 +0100

    Run Django project

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,13 +1,14 @@
 import requests
 
-endpoint = "https://www.github.com"
+endpoint = "http://localhost:8000/"
 
 get_response = requests.get(endpoint)
 print(get_response.text) # print raw text response
+print(get_response.status_code)
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dict
 
 # print(get_response.json())
-print(get_response.status_code)
+# print(get_response.status_code)
