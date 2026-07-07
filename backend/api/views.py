18fa1ef5e4f3be2fc26657212f578a10348fabea
kkamara <kamaracomputers@gmail.com>
2026-07-07 17:20:13 +0100

    Django model instance to dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,27 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import json
 from products.models import Product
+from django.forms.models import model_to_dict
+
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-        # model instance (model_data)
-        # turn a Python dict
-        # return JSON to my client
+        data = model_to_dict(
+            model_data,
+            fields=[
+                "id",
+                "title",
+                "price",
+            ],
+        )
     return JsonResponse(data)
+    #     print(data)
+    #     data = dict(data)
+    #     json_data_str = json.dumps(data)
+    #     print(json_data_str)
+    # return HttpResponse(json_data_str, headers={"Content-Type": "application/json"})
+    # My notes:
+    #   The price is a decimal field, so it needs to be converted for JSON serialization.
+    #   This is manual work that is tedious and error-prone.
