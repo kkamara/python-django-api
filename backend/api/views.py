ca233cba861cfdb74e1d9c53af29fc115677b702
kkamara <kamaracomputers@gmail.com>
2026-07-10 12:42:01 +0100

    Django Rest Framework model serialisers

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ from products.models import Product
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
 
 
 @api_view(["GET"])
@@ -9,15 +10,17 @@ def api_home(request, *args, **kwargs):
     """
     DRF API View
     """
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(
-            model_data,
-            fields=[
-                "id",
-                "title",
-                "price",
-            ],
-        )
+    if instance:
+        # data = model_to_dict(
+        #     instance,
+        #     fields=[
+        #         "id",
+        #         "title",
+        #         "price",
+        #         "sale_price",
+        #     ],
+        # )
+        data = ProductSerializer(instance).data
     return Response(data)
---
+++ backend/products/serializers.py
@@ -0,0 +1,18 @@
+from .models import Product
+from rest_framework import serializers
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    my_discount = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = Product
+        fields = [
+            "title",
+            "content",
+            "price",
+            "sale_price",
+            "my_discount",
+        ]
+    def get_my_discount(self, obj):
+        return obj.get_discount()
+    
