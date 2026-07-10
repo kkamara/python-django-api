3a3be4e49a3de7c25bc52290a965f5ed170730b7
kkamara <kamaracomputers@gmail.com>
2026-07-10 15:34:06 +0100

    Using function-based views for Create Retrieve or List

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
@@ -9,7 +13,6 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
-        print(serializer.validated_data)
         title = serializer.validated_data.get("title")
         content = serializer.validated_data.get("content") or None
         if content is None:
@@ -24,10 +27,41 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     # lookup_field = "pk" # The default
 
 
-class ProductListAPIView(generics.ListAPIView):
-    """
-    Not gonna use this method.
-    """
+# class ProductListAPIView(generics.ListAPIView):
+#     """
+#     Not gonna use this method.
+#     """
 
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+
+@api_view(["GET", "POST"])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if "GET" == method:
+        if pk is not None:
+            # detail view
+            obj = get_object_or_404(Product, pk=pk)
+            # many=False is the default
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+        # url_args
+        # get request -> detail view
+        # list view
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if "POST" == method:
+        # create an item
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get("title")
+            content = serializer.validated_data.get("content") or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid": "not good data"}, status=400)
