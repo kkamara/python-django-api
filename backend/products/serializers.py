747333488cb46ff26b47bf617b45f9d4039bf08c
kkamara <kamaracomputers@gmail.com>
2026-07-12 14:22:28 +0100

    Model Serializer create & update methods

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -8,13 +8,18 @@ class ProductSerializer(serializers.ModelSerializer):
     edit_url = serializers.SerializerMethodField(read_only=True)
     # Displays the full URL for viewing a product.
     # Only works on a `serializers.ModelSerializer`
-    url = serializers.HyperlinkedIdentityField(view_name="product_detail", lookup_field="pk")
+    url = serializers.HyperlinkedIdentityField(
+        view_name="product_detail", lookup_field="pk"
+    )
+    # Without write_only it will error because the field is not in the model. It will be used for creating a product.
+    # email = serializers.EmailField(write_only=True)
 
     class Meta:
         model = Product
         fields = [
             "url",
             "edit_url",
+            # "email",
             "pk",
             "title",
             "content",
@@ -23,11 +28,24 @@ class ProductSerializer(serializers.ModelSerializer):
             "my_discount",
         ]
 
+    # def create(self, validated_data):
+    #     # return Product.objects.create(**validated_data)
+    #     # email = validated_data.pop("email")
+    #     obj = super().create(validated_data)
+    #     # print(email, obj)
+    #     return obj
+    
+    # def update(self, instance, validated_data):
+    #     # instance.title = validated_data.get("title", instance.title)
+    #     # return instance
+    #     email = validated_data.pop("email", None) # Just to ensure the email gets removed from `validated_data`
+    #     return super().update(instance, validated_data)
+
     def get_edit_url(self, obj):
         """
         Display the full URL for editing the product.
         """
-        request = self.context.get("request") # self.request
+        request = self.context.get("request")  # self.request
         if request is None:
             return None
         return reverse("product_edit", kwargs={"pk": obj.pk}, request=request)
@@ -36,6 +54,7 @@ class ProductSerializer(serializers.ModelSerializer):
         if not isinstance(obj, Product):
             return None
         return obj.get_discount()
+
 
 # Tutorial creator's note:
 # Maybe we would want a ProductDetailSerializer
