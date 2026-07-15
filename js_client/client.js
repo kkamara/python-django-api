65aee6f9121ba1e2dbf103e8a20325a0ed301daf
kkamara <kamaracomputers@gmail.com>
2026-07-15 17:06:51 +0100

    Using JWT with JS client

--- js_client/client.js
+++ js_client/client.js
@@ -1,3 +1,4 @@
+const contentContainer = document.getElementById('id-content-container');
 const loginForm = document.getElementById('login-form');
 const baseEndpoint = "http://localhost:8000/api";
 
@@ -18,11 +19,56 @@ function handleLogin(event) {
         body: JSON.stringify(loginObjectData),
     };
     fetch(loginEndpoint, options)
-        .then(res => {
-            return res.json();
-        })
-        .then(res => {
-            console.log(res);
+        .then(res => res.json())
+        .then(authData => handleAuthData(authData, getProductList))
+        .catch(err => alert(err));
+}
+
+function handleAuthData(authData, callback) {
+    localStorage.setItem("access", authData.access);
+    localStorage.setItem("refresh", authData.refresh);
+    if (callback) {
+        callback();
+    }
+}
+
+function writeToContainer(data) {
+    if (contentContainer) {
+        contentContainer.innerHTML = "<pre>" + JSON.stringify(data, null, 4) + "</pre>";
+    }
+}
+
+function getFetchOptions(method, body) {
+    return {
+        method: null === method ? "GET" : method,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem("access")}`,
+        },
+        body: body ? body : null,
+    };
+}
+
+function isTokenNotValid(jsonData) {
+    if (jsonData.code && "token_not_valid" === jsonData.code) {
+        alert("Please login again")
+        return false
+    }
+    return true
+}
+
+function getProductList() {
+    const endpoint = `${baseEndpoint}/products/`;
+    const options = getFetchOptions();
+    fetch(endpoint, options)
+        .then(res => res.json())
+        .then(data => {
+            const validData = isTokenNotValid(data);
+            if (validData) {
+                writeToContainer(data);
+            }
         })
         .catch(err => alert(err));
 }
+
+getProductList();
